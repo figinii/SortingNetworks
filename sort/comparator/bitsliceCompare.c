cc8d19b87c684f8da41db83816ba3de1f8928ab8
figinii <matteo5.figini@mail.polimi.it>
2024-08-14 01:16:33 +0200

    feat: bitSlicing setup

---
+++ sort/comparator/bitsliceCompare.c
@@ -0,0 +1,5 @@
+#include "./bitsliceCompare.h"
+
+void bitsliceCompare(int32 firstIndex, int32 secondIndex, int32* arr){
+    
+}
---
+++ sort/comparator/bitsliceCompare.h
@@ -0,0 +1,3 @@
+#include "../../utils/bitslicingUtils.h"
+
+void bitsliceCompare(int32 firstIndex, int32 secondIndex, int32* arr);
